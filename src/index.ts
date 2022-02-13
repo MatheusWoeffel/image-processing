edd30f805fbbd1026ed63ba6f3ca2f1d87ae66af
MatheusWoeffel <mwcamargo@inf.ufrgs.br>
2022-02-13 14:34:21 -0300

    Simple ui

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,72 @@
-import { readFileSync, writeFileSync } from "fs";
+import {
+  QMainWindow,
+  QWidget,
+  QPushButton,
+  FlexLayout,
+  QLabel,
+  QPixmap,
+  QFileDialog,
+  FileMode,
+} from "@nodegui/nodegui";
+import { readFileSync } from "fs";
 import { decode, encode } from "jpeg-js";
+import { Image } from "./image-operations/types";
 import { convertToGrayScale } from "./image-operations/convertToGrayscale";
-import { mirrorHorizontally } from "./image-operations/mirrorHorizontally";
-import { mirrorVertically } from "./image-operations/mirrorVertically";
-import { quantizeImage } from "./image-operations/quantizeImage";
 
-async function main() {
-  const data = readFileSync(
-    "C:/git/image-processing/assets/photo_2022-02-13_00-18-35.jpg"
-  );
-  const result = decode(data);
+const win = new QMainWindow();
+const center = new QWidget();
+const getPictureBtn = new QPushButton();
+const convertToGreyscaleBtn = new QPushButton();
 
-  const newImage = mirrorVertically(result, 4);
-  const encodedJpeg = encode(newImage);
-  writeFileSync("./assets/foo.jpg", encodedJpeg.data);
+let imageData: Image | undefined;
+
+//----------
+getPictureBtn.setText("Select a photo");
+getPictureBtn.addEventListener("clicked", () => {
+  const fileDialog = new QFileDialog();
+  fileDialog.setFileMode(FileMode.AnyFile);
+  fileDialog.setNameFilter("Images (*.jpg)");
+  fileDialog.exec();
+
+  const selectedFiles = fileDialog.selectedFiles();
+  const imageSelected = selectedFiles[0];
+  const imageBuffer = readFileSync(imageSelected);
+  imageData = decode(imageBuffer);
+
+  const originalPictureWin = new QMainWindow();
+  const label = new QLabel();
+  const image = new QPixmap();
+  image.load(imageSelected);
+  label.setPixmap(image);
+  originalPictureWin.setCentralWidget(label);
+  originalPictureWin.show();
+});
+
+//--------------
+convertToGreyscaleBtn.setText("Convert to greyscale");
+convertToGreyscaleBtn.addEventListener("clicked", () => {
+  if (imageData) {
+    const newImage = convertToGrayScale(imageData, 4);
+    const newImageEncoded = encode(newImage);
+    displayNewImageWindow(newImageEncoded);
+  }
+});
+
+center.setLayout(new FlexLayout());
+center.layout?.addWidget(getPictureBtn);
+center.layout?.addWidget(convertToGreyscaleBtn);
+center.setInlineStyle(`width: 400; height: 400;`);
+win.setCentralWidget(center);
+win.show();
+win.setFixedSize(400, 400);
+(global as any).win = win;
+
+function displayNewImageWindow(imageData: Image) {
+  const originalPictureWin = new QMainWindow();
+  const label = new QLabel();
+  const image = new QPixmap();
+  image.loadFromData(imageData.data);
+  label.setPixmap(image);
+  originalPictureWin.setCentralWidget(label);
+  originalPictureWin.show();
 }
-
-main();
